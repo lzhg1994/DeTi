e80ae08a3f36a19007330ed0ccca084431b8658a
lee <lee@leedeMacBook-Pro.local>
2015-07-14 11:43:38 +0800

    Initial Commit

---
+++ DeTi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeTi
+//
+//  Created by lee on 15/7/14.
+//  Copyright (c) 2015年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
